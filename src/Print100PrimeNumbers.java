a57e8a3afeabc31b534098783a1c041792d0f3bc
shaikhrafi28 <45448097+shaikhrafi28@users.noreply.github.com>
2019-01-11 19:02:42 +0530

    Add files via upload

---
+++ src/Print100PrimeNumbers.java
@@ -0,0 +1,27 @@
+package InterviewQuestions;
+
+public class Print100PrimeNumbers {
+
+	public static void main(String[] args) {
+
+		for (int i = 2; i <= 100; i++) {
+			boolean flag = true;
+			for (int j = 2; j <= i - 1; j++) {
+				if (i % j == 0) {
+					flag = false;
+					break;
+
+				}
+
+			}
+			if (flag == true) {
+				System.out.print(i);
+
+				System.out.println(" ");
+
+			}
+		}
+
+	}
+
+}
