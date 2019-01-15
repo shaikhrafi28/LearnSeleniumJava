b2ed478e49ea05321ee3a9f1fbaf7615ea389398
shaikhrafi08 <33877774+shaikhrafi08@users.noreply.github.com>
2019-01-16 01:28:11 +0530

    4

---
+++ src/InterviewQuestions/FindFactorial.java
@@ -0,0 +1,33 @@
+package InterviewQuestions;
+
+public class FindFactorial {
+
+	public static void main(String[] args) {
+
+		System.out.println(FactorialOf(4));
+		System.out.println(RFactorialOf(4));
+
+	}
+
+//Without Recursive
+	public static int FactorialOf(int num) {
+		int fact = 1;
+		if (num == 0)
+			return 1;
+		for (int i = 1; i <= num; i++) {
+
+			fact = fact * i;
+		}
+		return fact;
+	}
+
+// With Recursive
+	
+	public static int RFactorialOf(int num) {
+		if(num==0)
+			return 1;
+		else
+			return (num*RFactorialOf(num-1));
+	}
+
+}
