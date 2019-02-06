b20195f3aec5a2c5e722e67032967be2f81817ab
shaikhrafi28 <Bhalubunny@08>
2019-02-06 18:35:41 +0530

    3

---
+++ practice.selenium.java.oops/practice/testbase/TestBaseSetup.java
@@ -0,0 +1,95 @@
+package practice.testbase;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestBaseSetup {
+
+	public static WebDriver driver;
+	public static Properties prop;
+	public static EventFiringWebDriver e_driver;
+
+	public TestBaseSetup() {// constructor
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(
+					System.getProperty("user.dir") + "/src/main/java/com/caqhgroups" + "/qa/config/config.properties");
+
+			prop.load(ip);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void initialization(String browserName) {
+
+		if (browserName.equals("chrome")) {
+
+			// WebDriverManager.chromedriver().setup();
+			System.setProperty("webdriver.chrome.driver", "C:/Selenium/drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+			System.out.println("Chrome Invoked");
+
+		} else if (browserName.equals("FF")) {
+			System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			System.out.println("Firefox Invoked");
+		} else if (browserName.equals("IE")) {
+			System.setProperty("webdriver.ie.driver", "C:\\Selenium\\drivers\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+			System.out.println("IE Invoked");
+		}
+
+		else {
+			System.out.println("No Browser found");
+
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+
+	}
+
+	public static void giveGroupsUrl() {
+		driver.get(prop.getProperty("GroupsUrl"));
+
+	}
+
+	public static void giveProviewUrl() {
+		driver.get(prop.getProperty("POurl"));
+
+	}
+
+	/*
+	 * public static void ewait(String ide) { WebDriverWait wait = new
+	 * WebDriverWait(driver,30); WebElement aboutMe; aboutMe=
+	 * wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(ide))); }
+	 */
+	public void closebrowser() {
+		// Closing via JavaScript
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.close()");
+
+	}
+}
