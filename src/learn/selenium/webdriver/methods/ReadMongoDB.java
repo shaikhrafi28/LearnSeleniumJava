0f0b3f0e32e7796ffc5293841a7c6906c640896e
shaikhrafi28 <Bhalubunny@08>
2019-02-07 18:39:18 +0530

    1

--- src/learn/selenium/webdriver/methods/ReadMongoDB.java
+++ src/learn/selenium/webdriver/methods/ReadMongoDB.java
@@ -28,32 +28,28 @@ public class ReadMongoDB {
 
 		// MongoClientURI uri = new
 		// MongoClientURI("mongodb://user1:pwd1@host1/?authSource=db1");
-	
+
 		MongoClientURI uri = new MongoClientURI(
-			"mongodb://grpsuatmdb1.caqhgrps.local:27017,grpsuatmdb2.caqhgrps.local:27017,grpsuatmdb3.caqhgrps.local:27017/?replicaSet=lmdbrpset");
+				// mongodb://myDBReader:D1fficultP%40ssw0rd@mongodb0.example.com:27017,mongodb1.example.com:27017,mongodb2.example.com:27017/admin?replicaSet=myRepl
+				"mongodb://shaikh.mohammad.rafi:cOXoh%2F6itIUgag==@grpsuatmdb1.caqhgrps.local:27017,grpsuatmdb2.caqhgrps.local:27017,grpsuatmdb3.caqhgrps.local:27017/?replicaSet=lmdbrpset");
 		MongoClient client = new MongoClient(uri); // with default server and port adress
-		MongoDatabase database = client.getDatabase("grpssitmdb1");
+		MongoDatabase database = client.getDatabase("grpsdevmdb1");
 
-		MongoCollection<Document> collection = database.getCollection("Groups");
-		FindIterable<Document> iterDoc = collection.find();
+		MongoCollection<Document> collection = database.getCollection("Providers");
+//CTRL+SHIFT+/ - COMMENT		
+/*		FindIterable<Document> iterDoc = collection.find();
 		int i = 1;
-
 		// Getting the iterator
 		Iterator it = iterDoc.iterator();
-
 		while (it.hasNext()) {
 			System.out.println(it.next());
 			i++;
 		}
-
-		/*
-		 * List<Document> documents = (List<Document>) collection.find().into( new
-		 * List<Document>());
-		 * 
-		 * for(Document document : documents){ System.out.println(document);
-		 * 
-		 * }
-		 */
+*/
+		//Listing the available collections in mongoDB
+		for (String name : database.listCollectionNames()) { 
+	         System.out.println(name); 
+	      } 
 	}
 
 }
